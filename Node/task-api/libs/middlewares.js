3982120a85937a358c0fcaeab7de5dc624cf7d40
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-08-05 08:41:04 +0200

    Nodejs

--- Node/task-api/libs/middlewares.js
+++ Node/task-api/libs/middlewares.js
@@ -1,4 +1,11 @@
 module.exports = APP => {
     APP.set('json spaces', 4);
     APP.set('port', 3000);
+    APP.use(bodyParser.json());
+    APP.use((req, res, next) => {
+        if (req.body) {
+            delete req.body.id;
+            next();
+        }
+    });
 };
--- Node/task-api/routes/tasks.js
+++ Node/task-api/routes/tasks.js
@@ -1,8 +1,42 @@
 module.exports = APP => {
     const Tasks = APP.db.models.Tasks;
-    APP.get("/tasks", (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json({tasks: tasks});
+    APP.route('/tasks')
+        .get((req, res) => {
+            Tasks.findAll({})
+                .then(result => res.json(result)).catch(error => {
+                res.status(412).json({msg: error.message});
+            });
+        })
+        .post((req, res) => {
+            Tasks.create(req.body).then(result => res.json(result)).catch(error => {
+                res.status(412).json({msg: error.message});
+            });
         });
-    });
+    APP.route('/tasks/:id')
+        .get((req, resp, next) => {
+            Tasks.findOne({where: req.params})
+                .then(result => {
+                    if (result) {
+                        res.json(result);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                })
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                });
+        })
+        .put((req, res) => {
+            delete req.body.id;
+            Tasks.update(req.body, {where: req.params}).then(result => res.sendStatus(204)).catch(error => {
+                res.status(412).json({msg: error.message});
+            });
+
+        })
+        .delete((req, res) => {
+            Tasks.destroy({where: req.params}).then(result => res.sendStatus(204)).catch(error => {
+                res.status(412).json({msg: error.message});
+            });
+
+        });
 };
